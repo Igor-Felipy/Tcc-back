6bab95855de63e082015404f2aa6915f014be245
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-04 18:34:40 -0300

    adicionando rotas esquecidas

--- app/controllers/post/routes.py
+++ app/controllers/post/routes.py
@@ -2,6 +2,7 @@ from . import posts
 from ..auth.authenticate import jwt_required
 
 
+#posts routes
 @posts.route("/post",methods=["POST"])
 @jwt_required
 def post(current_user):
@@ -23,3 +24,47 @@ def edit_post(current_user):
 @jwt_required
 def delete_post(current_user):
     return "delete_post route"
+
+
+@posts.route("/post/comments", methods=["POST"])
+@jwt_required
+def commments(current_user):
+    return "all the coments from a post"
+
+
+
+
+
+#comments routes
+@posts.route("/post/new_comment", methods=["POST"])
+@jwt_required
+def New_comment(current_user):
+    return "Create a comment"
+
+@posts.route("/post/qtd_comment", methods=["POST"])
+@jwt_required
+def qtd_comment(current_user):
+    return "Create a comment"
+    
+
+
+
+
+
+#likes routes
+@posts.route("/post/like_qtd", methods=["POST"])
+@jwt_required
+def like_qtd(current_user):
+    return "quantity likes route"
+
+
+@posts.route("/post/new_like", methods=["POST"])
+@jwt_required
+def new_like(current_user):
+    return "New_like route"
+
+
+@posts.route("/post/cancel_like", methods=["POST"])
+@jwt_required
+def cancel_like(current_user):
+    return "cancel_like route"
