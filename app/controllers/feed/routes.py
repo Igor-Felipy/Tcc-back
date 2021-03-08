c41d802c82c67cadfebe002fcb8abfb81b4467e0
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-07 21:32:56 -0300

    iniciando a rota

--- app/controllers/feed/routes.py
+++ app/controllers/feed/routes.py
@@ -1,7 +1,11 @@
 from . import feed
 from ..auth.authenticate import jwt_required
+from app.models.tables import Post
+from flask import jsonify
+
 
 @feed.route("/feed", methods=["POST"])
 @jwt_required
 def feed(current_user):
-    return "feed route"
+    post = Post.query.filter_by().all()
+    return jsonify(post)
