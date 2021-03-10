fa43255dea97d47b43ff54289a88291c0ed1e277
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-10 00:47:26 -0300

    fin

--- app/controllers/details/routes.py
+++ app/controllers/details/routes.py
@@ -1,8 +1,28 @@
 from . import post_detail
 from ..auth.authenticate import jwt_required
+from flask import request, jsonify
+from app.models.tables import Detail
 
 
 @post_detail.route("/detail", methods=["POST"])
 @jwt_required
 def post_detail(current_user):
+    post_id = request.json['post_id']
+    detail = Detail.query.filter_by(post_id=post_id).first()
+    if detail is not None:
+        try:
+            return jsonify({
+                "id":detail.id,
+                "image_feels":detail.image_feels,
+                "caption_feels":detail.caption_feels,
+                "post_id":detail.post_id
+            })
+        except:
+            return jsonify({"error":"something went wrong!"})
+    else:
+        try:
+            #criar os detalhes
+            pass
+        except:
+            return jsonify({"error":"try again later!"})
     return "details route"
