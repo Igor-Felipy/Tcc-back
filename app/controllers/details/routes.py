61934d3128bd70824a1a2ba30b003ff8463aabcb
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-11 19:25:51 -0300

    corrigindo a rota de detalhes

--- app/controllers/details/routes.py
+++ app/controllers/details/routes.py
@@ -3,6 +3,7 @@ from ..auth.authenticate import jwt_required
 from flask import request, jsonify
 from app.models.tables import Detail, Post
 from app.controllers.details.leia import SentimentIntensityAnalyzer 
+from app import db
 
 
 @post_detail.route("/detail", methods=["POST"])
@@ -26,5 +27,16 @@ def post_detail(current_user):
             score = SentimentIntensityAnalyzer.polarity_scores(post.caption)
 
             new_detail = Detail(None,score.compound,post_id)
+            db.session.add(new_detail)
+            db.session.commit()
+
+            new_response = Detail.query.filter_by(post_id=post_id).first()
+
+            return jsonify({
+                "id":new_response.id,
+                "image_feels":new_response.image_feels,
+                "caption_feels":new_response.caption_feels,
+                "post_id":new_response.post_id
+            })
         except:
             return jsonify({"error":"try again later!"})
