d787f2edef21897bc80b8a88a07f1ad47e2a9233
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-12 15:54:54 -0300

    escrevendo o algoritmo de feed

--- app/controllers/feed/routes.py
+++ app/controllers/feed/routes.py
@@ -8,13 +8,13 @@ from app import db
 @feed.route("/feed", methods=["POST"])
 @jwt_required
 def feed(current_user):
-    posts = db.session.query(
-        Post, Follow
-        ).filter(
-            Follow.user_id == current_user
-        ).filter(
-            Post.user_id == Follow.follower_id
-        )
+    posts = db.session.connection().execute(f"""
+        SELECT * FROM posts   
+        INNER JOIN follow
+        ON posts.user_id = follow.user_id
+        INNER JOIN users
+        ON follow.follower_id = {current_user.id};
+    """)
     post_converted = dict()
     for post in posts:
         post_converted.update({
