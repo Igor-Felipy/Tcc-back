fafb029319fd707c10550039cbddf5b057abcbe7
Igor-Felipy <igor.felipy123@gmail.com>
2021-10-18 20:25:51 -0300

    atualizando o banco

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'BIG_SECRET_KEY'
+#mysql://ba4b428d805d74:eb41e9fb@us-cdbr-east-04.cleardb.com/heroku_de1cf8160b06958?reconnect=true
